9ae0e3d6e5362c569f3f906a228bee74644c25eb
MatthieuGA <matthieu.guillet-arnaud@epitech.eu>
2025-03-03 11:28:34 +0100

    [ADD] base main architecture

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,31 @@ void help()
     "\tn\tmaximum length of friendship paths\n");
 }
 
+int links()
+{
+    printf("links\n");
+    return 0;
+}
+
+int plots()
+{
+    printf("plots\n");
+    return 0;
+}
+
 int main(int ac, char **av) {
     if (ac == 2 && (std::string(av[1]) == "--help" || std::string(av[1]) == "-h")) {
         help();
         return 0;
+    }
+    if (ac != 5 || (std::string(av[1]) != "--links" && std::string(av[1]) != "--plots")) {
+        std::cerr << "Invalid arguments" << std::endl;
+        return 84;
+    }
+    if (std::string(av[1]) == "--links") {
+        return links();
+    } else {
+        return plots();
     }
     return 0;
 }
